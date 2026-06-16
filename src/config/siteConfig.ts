7e2edd804c30ee7791e785d4f342c86ed9355bd4
h-lyf <57041119+h-lyf@users.noreply.github.com>
2026-06-16 21:19:46 +0800

    new file:   public/favicon/avatar.ico 	new file:   src/assets/images/logo.webp 	deleted:    src/assets/logo.webp 	modified:   src/config/siteConfig.ts

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -56,7 +56,7 @@ export const siteConfig: SiteConfig = {
 	favicon: [
 		{
 			// 图标文件路径
-			src: "/favicon/favicon.ico",
+			src: "/favicon/avatar.ico",
 			// 可选，指定主题 'light' | 'dark'
 			// theme: "light",
 			// 可选，图标大小
