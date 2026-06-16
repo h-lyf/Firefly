7e1ee0cdfef0704dfacb23237d8187ef678237b4
h-lyf <57041119+h-lyf@users.noreply.github.com>
2026-06-16 13:45:42 +0800

    modified:   src/config/fontConfig.ts

--- src/config/fontConfig.ts
+++ src/config/fontConfig.ts
@@ -7,7 +7,7 @@ export const fontConfig: FontConfig = {
 	// 是否预加载字体文件
 	preload: true,
 	// 当前选择的字体，支持多个字体组合
-	selected: ["inter"],
+	selected: ["misans-normal"],
 
 	// 字体列表
 	// 推荐使用可靠的 CDN 服务商提供的字体链接，它天然做了按需分片加载，且性能较好
